f3e282c090e6a6f7c758c15ed46b360f1a6314c2
Marta Grana <marta.grana.fdez@gmail.com>
2024-07-24 11:30:47 +0200

    6.133 ngAfertViewInit ngAfterContentInit

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, ContentChild, ElementRef, inject, input, ViewEncapsulation } from '@angular/core';
+import { AfterContentInit, Component, contentChild, ContentChild, ElementRef, inject, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -12,15 +12,20 @@ import { Component, ContentChild, ElementRef, inject, input, ViewEncapsulation }
     '(click)': 'onClick()',
   }
 })
-export class ControlComponent {
+export class ControlComponent implements AfterContentInit {
 
   label = input.required<string>();
   private el = inject(ElementRef);
-  @ContentChild('input') private control?: ElementRef<HTMLInputElement | HTMLTextAreaElement>
 
+  //  @ContentChild('input') private control?: ElementRef<HTMLInputElement | HTMLTextAreaElement>
+  private control = contentChild<ElementRef<HTMLInputElement | HTMLTextAreaElement>>('input');
+
+  ngAfterContentInit() {
+    //...
+  }
   onClick() {
     console.log('Clicked!');
     console.log(this.el);
-    console.log(this.control);
+    console.log(this.control());
   }
 }
