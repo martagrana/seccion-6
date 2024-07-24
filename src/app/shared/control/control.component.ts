4cbd59acabb1f8f1cce5f13009cd5d73df952b84
Marta Grana <marta.grana.fdez@gmail.com>
2024-07-24 10:35:51 +0200

    6.132 ViewChild vs ContentChild

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, inject, input, ViewEncapsulation } from '@angular/core';
+import { Component, ContentChild, ElementRef, inject, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -16,9 +16,11 @@ export class ControlComponent {
 
   label = input.required<string>();
   private el = inject(ElementRef);
+  @ContentChild('input') private control?: ElementRef<HTMLInputElement | HTMLTextAreaElement>
 
   onClick() {
     console.log('Clicked!');
     console.log(this.el);
+    console.log(this.control);
   }
 }
