bbb34c91f42d4077ddb2dd9c59e215960df63f51
Marta Grana <marta.grana.fdez@gmail.com>
2024-07-24 14:18:39 +0200

    6.137-6.138-6.139 type models, outputs, template for

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, viewChild, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, output, Output, viewChild, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
@@ -14,6 +14,8 @@ export class NewTicketComponent implements AfterViewInit {
 
   //@ViewChild('form') private form?: ElementRef<HTMLFormElement>;
   private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+  //@Output() add= new EventEmitter<{title:string;text:string}>();
+  add = output<{ title: string; text: string }>();
 
   ngAfterViewInit() {
     console.log('AFTER VIEW INIT');
@@ -21,8 +23,7 @@ export class NewTicketComponent implements AfterViewInit {
   }
 
   onSubmit(title: string, ticketText: string) {
-    console.log(title);
-    console.log(ticketText);
+    this.add.emit({ title: title, text: ticketText });
     this.form().nativeElement.reset();
   }
 }
